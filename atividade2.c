5b1a5feb1072a3c9f3522bb80eb40c484cd39470
PlagueBelphegor <91290592+DonquixoteDonflamingo@users.noreply.github.com>
2024-03-26 09:57:03 -0300

    Add files via upload

--- atividade2.c
+++ atividade2.c
@@ -3,7 +3,10 @@
 
 int main(int argc, char* argv[])
 {
-    printf("Atividade 1");
+    printf("\tAtividade 2\n");
+    printf("\n1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("argc = %d\n", argc);
+    printf("argv[3] = %s\n", argv[3]);
 
 exit(0);
 }
