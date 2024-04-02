163ca1961233b8f3b7258dc6da3bdd8aa0504041
PlagueBelphegor <91290592+DonquixoteDonflamingo@users.noreply.github.com>
2024-04-02 11:24:35 -0300

    Add files via upload

--- JogodaVelhaBroCode.c
+++ JogodaVelhaBroCode.c
@@ -5,6 +5,7 @@
 
 char board[3][3];
 const char PLAYA = 'X';
+const char CPU = 'O';
 
 void resetBoard();
 void printBoard();
@@ -16,21 +17,48 @@ void printWinner(char);
 
 int main()
 {
+char winner = ' ';
 
+    resetBoard();
+while(winner == ' ' && checkFreeSpaces() != 0)
+{    
+    printBoard();
+}
 return 0;
 }
 
 void resetBoard()
 {
-
+for(int i = 0; i < 3; i++)
+{
+    for(int j = 0; j < 3; j++)
+    {
+        board[i][j] = ' ';
+    }
+}
 }
 void printBoard()
 {
-
+    printf(" %c | %c | %c", board[0][0], board[0][1], board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c", board[1][0], board[1][1], board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c", board[2][0], board[2][1], board[2][2]);
+    printf("\n");
 }
 int checkFreeSpaces()
 {
+int freeSpaces = 9;
+for(int i = 0; i < 3; i++)
+{
+    for(int j = 0; j < 3; j++)
+    {
+        if(board[i][j] != ' ')
+        {
 
+        }
+    }
+}
 }
 void playerMove()
 {
---
+++ NUMSEI.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+char txt [100];
+FILE arq_txt;
+
+    arq_txt = fopen("arq.txt", "r");
+if(arq_txt == NULL)
+{
+    printf("ERRO AO ABRIR O ARQUIVO SOLICITADO");
+    exit(1);
+}
+while(fscanf(arq_txt, "%[^\n]\n", &txt) != EOF)
+{
+    printf("%s\n", txt);
+}
+
+fclose(arq_txt);
+exit(0);
+}
